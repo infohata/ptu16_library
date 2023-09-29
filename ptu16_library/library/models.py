eedd7e65472e5bc12851f037d42a1cc464756192
Kestutis Januskevicius <infohata@gmail.com>
2023-09-29 14:36:34 +0300

    admin enhanced

--- ptu16_library/library/models.py
+++ ptu16_library/library/models.py
@@ -33,6 +33,10 @@ class Author(models.Model):
 
     def get_absolute_url(self):
         return reverse("author_detail", kwargs={"pk": self.pk})
+    
+    def display_books(self):
+        return ", ".join(book.title for book in self.books.all()[:3])
+    display_books.short_description = _('books')
 
 
 class Book(models.Model):
@@ -60,6 +64,10 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("book_detail", kwargs={"pk": self.pk})
+    
+    def display_genre(self):
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = _('genre')
 
 
 LOAN_STATUS = (
