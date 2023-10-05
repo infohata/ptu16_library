b20d50c3c0b1a82bfaedc16b3aaae9db5ac43065
Kestutis Januskevicius <infohata@gmail.com>
2023-10-05 08:28:40 +0300

    HTTP sessions, visits counter in index

--- ptu16_library/library/urls.py
+++ ptu16_library/library/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('author/<int:pk>/', views.author_detail, name='author_detail'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-
 ]
